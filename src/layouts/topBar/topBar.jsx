130aaca4eb4ead7fb173db8a67a9071c426f553c
M-SE0K <seg082911@gmail.com>
2026-01-06 18:33:32 +0900

    feat: topBar 대표전화, 공지사항 삭제

--- src/layouts/topBar/topBar.jsx
+++ src/layouts/topBar/topBar.jsx
@@ -5,11 +5,6 @@ import './topBar.css';
 function TopBar() {
   return (
     <div className="top-bar">
-      <div className="top-bar-left">
-        <span className="top-bar-text">전북대학교 공과대학 학생회</span>
-        <span className="top-bar-divider">|</span>
-        <span className="top-bar-text">대표전화: 063-270-2283</span>
-      </div>
       <div className="top-bar-right">
         <Link to="/contact/report" className="top-bar-link">민원접수</Link>
         <span className="top-bar-divider">|</span>
